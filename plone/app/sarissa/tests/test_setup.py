ec8577e27f8e3a511e338fd92853aa31ac7da6e2
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-26 10:48:15 +0100

    remove upgrade step must be in plone.app.upgrades

--- plone/app/sarissa/tests/test_setup.py
+++ plone/app/sarissa/tests/test_setup.py
@@ -12,20 +12,6 @@ class TestIntegration(base.TestCase):
         new_sarissa = jsregistry.getResource(JS)
         self.failUnless(new_sarissa.getEnabled())
 
-    def test_upgrade(self):
-        from Products.GenericSetup.upgrade import listUpgradeSteps
-        setup = self.portal.portal_setup
-        steps = listUpgradeSteps(setup, PROFILE, None)
-        step_id = steps[0]['id']
-        request = self.portal.REQUEST
-        request.form['upgrades'] = [step_id]
-        request.form['profile_id'] = PROFILE
-        setup.manage_doUpgrades()
-        
-        jsregistry = self.portal.portal_javascripts
-        sarissa = jsregistry.getResource('sarissa.js')
-        self.failUnless(sarissa is None)
-
 
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
--- plone/app/sarissa/upgrades.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-def remove_plone3rdParty_sarissa(context):
-    """This is the future upgrade that will be applied to Plone"""
-    jsregistry = getToolByName(context, 'portal_javascripts')
-    jsregistry.unregisterResource('sarissa.js')
-    jsregistry.cookResources()
